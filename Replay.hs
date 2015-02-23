77dfaf0afb758c5be19e14721498191de55dbc30
Simon Robillard <simon.robillard@chalmers.se>
2015-02-23 17:52:46 +0100

    no progress on cut, just some cleanup in Replay

--- Replay.hs
+++ Replay.hs
@@ -28,9 +28,8 @@ instance (Monad m) => Monad (ReplayT m q r) where
     return x = ReplayT $ \t -> return (Right x, t)
     m >>=  k = ReplayT $ \t -> do (qora, t') <- runReplayT m t
                                   case qora of
-                                     (Right a) -> runReplayT (k a) t'
                                      (Left q) -> return (Left q, t')
-
+                                     (Right a) -> runReplayT (k a) t'
 
 -- | Extract a result of a monadic computation and add it to the trace.
 -- Require type a to be instances of Show and Read to be able to 
@@ -38,12 +37,12 @@ instance (Monad m) => Monad (ReplayT m q r) where
 
 {- liftR doesn't satisfy the monad transformer laws: 
 
-  (1)   lift . return = return
+  (1)   liftR . return <> return
 
    Our definition of return doesn't manipulate the trace, but liftR does.
 
 
-  (2)   lift (m >>= f) = lift m >>= (lift . f)
+  (2)   liftR (m >>= f) <> liftR m >>= (liftR . f)
 
    Here lift occurs once  on the left, and twice on the right.
    Using the same reasoning, on the LHS the trace is modified once, 
@@ -55,18 +54,17 @@ instance (Monad m) => Monad (ReplayT m q r) where
 
 -}
 liftR :: (Monad m, Show a, Read a) => m a -> ReplayT m q r a
-liftR input = ReplayT $ \t -> do
-               case todo t of
-                 [] -> do a <- input
-                          return (Right a, addResult (show a) t)
-                 (val:ts) -> case val of
-                               Answer a -> fail "liftR"
-                               Result str -> return (Right $ read str, visit t)
+liftR input = ReplayT $ \t ->
+              case todo t of
+                [] -> do a <- input
+                         return (Right a, addResult (show a) t)
+                ((Answer a):_) -> fail "liftR"
+                ((Result str):_) -> return (Right $ read str, visit t)
 
 -- | Extract either an answer or a question with a trace, wrapped in
 -- the underlying monad.
 run      :: (Monad m) => ReplayT m q r a -> Trace r -> m (Either (q, Trace r) a)
-run ra t = do (qora, t') <- (runReplayT ra) (resetTrace t)
+run ra t = do (qora, t') <- runReplayT ra (resetTrace t)
               case qora of
                 Left q -> return $ Left (q, t')
                 Right a -> return $ Right a
@@ -78,14 +76,20 @@ io = liftR
 -- | Try to answer the question with the trace.  If the trace is
 -- empty, return the question and the used trace.
 ask          :: (Monad m) => q -> ReplayT m q r r
-ask question = ReplayT $ \t -> do
+ask question = ReplayT $ \t ->
                  case todo t of
                    [] -> return (Left question, t)
-                   (val:ts) -> case val of
-                                 Answer a   -> return (Right a, visit t)
-                                 Result str -> fail $ "ask: " ++ str
+                   ((Answer a):_) -> return (Right a, visit t)
+                   ((Result str):_) -> fail $ "ask: " ++ str
 
--- Helper functions for manipulating traces
+cut :: (Monad m, Read a, Show a) => ReplayT m q r a -> ReplayT m q r a
+cut ra = ReplayT $ \t ->
+           case todo t of
+             [] -> runReplayT ra t
+             ((Answer a):_) -> undefined
+             ((Result str):_) -> undefined
+
+--Helper functions for manipulating traces
 
 emptyTrace :: Trace r
 emptyTrace = Trace [] []
@@ -102,17 +106,15 @@ addResult str (Trace v []) =  Trace ((Result str):v) []
 addAnswer                :: r ->Trace r -> Trace r
 addAnswer r (Trace v []) =  Trace ((Answer r):v) []
 
--- |Move the first todo element to the visited elements list
+-- | Move the first todo element to the visited elements list
 visit             :: Trace r -> Trace r
 visit (Trace v t) = case t of
                       [] -> Trace v t
                       (x:xs) -> Trace (x:v) xs
 
 -- | Extract the answers of a Trace
-getAnswers :: Trace r -> [r]
-getAnswers (Trace vis todo) = [getAnswer r | r <- vis, isAnswer r] 
-  where isAnswer (Answer _) = True
-        isAnswer _          = False
-        getAnswer (Answer r) = r
-        getAnswer _          = error "D:"
-
+getAnswers               :: Trace r -> [r]
+getAnswers (Trace vis _) =  reverse $ foldr consAns [] vis
+    where consAns (Answer r) rs = r:rs
+          consAns (Result _) rs = rs
+                      
